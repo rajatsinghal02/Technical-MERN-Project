cc236f70d89f3425affc2d066b9465920867a45f
Rajatsinghal02 <singhalrajat354@gmail.com>
2024-11-11 13:51:20 +0530

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const postContainer = document.getElementById('post-container');
+    const Container = document.getElementById('post-container');
     const loader = document.getElementById('loader');
         
     async function fetchPosts() {
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (posts.length > 0) {
                 posts.forEach(post => {
-                    const postElement = document.createElement('div');
-                    postElement.classList.add('post');
-                    postElement.innerHTML = `<h1>${post.id}</h1><h3>${post.title}</h3><p>${post.body}</p>`;
-                    postContainer.appendChild(postElement);
+                    const div = document.createElement('div');
+                    div.classList.add('post');
+                    div.innerHTML = `<h1>${post.id}</h1><h3>${post.title}</h3><p>${post.body}</p>`;
+                    Container.appendChild(postElement);
                 });
             } else {
                 window.removeEventListener('scroll', handleScroll);
